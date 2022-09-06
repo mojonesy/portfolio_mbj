2d48baccc13f7639bac3ebee702f021f39d6232a
Molly <mojonesycreative@gmail.com>
2022-09-05 23:55:49 -0400

    added photo modal zoom function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/* Nav */
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
@@ -10,3 +11,35 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     });
 });
+
+
+/* Image modals */
+const images = document.querySelectorAll(".photo__item img");
+let imgSrc;
+
+images.forEach((img) => {
+    img.addEventListener("click", (e) => {
+        imgSrc = e.target.src;
+
+        imgModal(imgSrc);
+    });
+});
+
+let imgModal = (src) => {
+    const modal = document.createElement("div");
+    modal.setAttribute("class", "modal");
+
+    document.querySelector(".main").append(modal);
+
+    const newImg = document.createElement("img");
+    newImg.setAttribute("src", src);
+
+    const closeBtn = document.createElement("i");
+    closeBtn.setAttribute("class", "fas fa-times closeBtn");
+
+    closeBtn.onclick = () => {
+        modal.remove();
+    };
+
+    modal.append(newImg, closeBtn);
+};
